a61a9c534b7fdfb5ff4001303aa4506128a9d530
MattyAtown <mattauton22@gmail.com>
2025-10-17 21:37:50 +0100

    Add files via upload

---
+++ Static/js/comic_intro.js
@@ -0,0 +1,5 @@
+window.onload = () => {
+  setTimeout(() => document.getElementById('panel1').style.opacity = 1, 1000);
+  setTimeout(() => document.getElementById('panel2').style.opacity = 1, 4000);
+  setTimeout(() => document.getElementById('panel3').style.opacity = 1, 7000);
+};
---
+++ Static/js/intro.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+    setTimeout(() => {
+        document.getElementById('codeIntro').style.display = 'none';
+        document.getElementById('digigonReveal').style.display = 'block';
+    }, 6000); // 6 seconds of intro verse
+};
