279fa0aae87c2b9d641d4ad706900561bdb68fdd
MattyAtown <mattauton22@gmail.com>
2025-10-22 21:19:10 +0100

    Delete static/js directory

--- static/js/intro.js
+++
@@ -1,6 +0,0 @@
-window.onload = () => {
-    setTimeout(() => {
-        document.getElementById('codeIntro').style.display = 'none';
-        document.getElementById('digigonReveal').style.display = 'block';
-    }, 6000); // 6 seconds of intro verse
-};
