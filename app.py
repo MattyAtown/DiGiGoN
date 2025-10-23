07b5f65dfd289d75f13bca7e3200bad2d671cc41
MattyAtown <mattauton22@gmail.com>
2025-10-23 20:48:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Home route
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Health check route (Render uses this to verify uptime)
 @app.route('/health')
 def health():
     return 'OK', 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
